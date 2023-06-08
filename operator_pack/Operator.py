b3334cc4d9935aa6fa6231829b758975471f52e4
Syed Asil Ali <asilshah17@gmail.com>
2023-06-08 10:32:53 +0500

    POS

--- operator_pack/Operator.py
+++ operator_pack/Operator.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 from datetime import datetime
 from kivy.lang import Builder
 
-Builder.load_file('operator_pack/Operator.kv')
+# Builder.load_file('operator_pack/Operator.kv')
 
 class OperatorWindow(BoxLayout):
     def __init__(self, **kwargs):
@@ -42,19 +42,24 @@ class OperatorWindow(BoxLayout):
             qty_input = self.ids.qty_input.text
 
 
+
             if qty_input == '':
                 qty_input = '1'
 
 
-            name = Label(text=target_code['Product_Description'], size_hint_x = 0.3, color = (0,0,0.45,1))
             code = Label(text=pcode, size_hint_x = 0.1, color = (0,0,0.45,1))
+            name = Label(text=target_code['Product_Description'], size_hint_x = 0.3, color = (0,0,0.45,1))
             model = Label(text= target_code['Model'], size_hint_x = 0.1, color = (0,0,.45,1) )
             disc = Label(text='0.00', size_hint_x = 0.1, color = (0,0,0.45,1))
             qty = Label(text=qty_input, size_hint_x = 0.1, color = (0,0,0.45,1))
             price= Label(text=target_code['Sale_Price'], size_hint_x = 0.1, color = (0,0,0.45,1))
 
-            details.add_widget(name)
+            total_price = int(qty_input) * float(target_code['Sale_Price'])
+            total = Label(text=str(total_price), size_hint_x=0.1,
+                          color=(0, 0, 0.45, 1))
+
             details.add_widget(code)
+            details.add_widget(name)
             details.add_widget(model)
             details.add_widget(disc)
             details.add_widget(qty)
@@ -64,7 +69,7 @@ class OperatorWindow(BoxLayout):
             pprice = float(price.text)
 
             pmodel = model.text
-            self.total += pprice
+            self.total += total_price
             self.ids.cur_product.text = pname
             self.ids.cur_price.text = str(pprice)
             purchaseTotal = '`\n\nTotal\t\t\t\t\t\t\t\t'+ str(self.total)
@@ -83,27 +88,27 @@ class OperatorWindow(BoxLayout):
                     ptarget = i
 
             if ptarget>=0:
-                qty_input= self.qty[ptarget] + qty_input
-                self.qty[ptarget] = qty_input
-                expr = '%s\t\tx\d\t'%(pname)
-                reexpr = pname+'\t\tx'+ qty_input + '\t'
-                nu_expr = re.sub(expr,reexpr,prev_text)
+                qty_input= int(self.qty[ptarget]) + int(qty_input)
+                self.qty[ptarget] = int(qty_input)
+                pqty = qty_input
+                print(pqty)
+                expr = '%s\t\tx\d\t'%(pmodel)
+                reexpr = pmodel+'\t\tx'+str(qty_input)+'\t'
+                nu_expr = re.sub(expr, reexpr, prev_text)
                 preview.text = nu_expr + purchaseTotal
             else:
                 self.cart.append(pcode)
                 self.qty.append(qty_input)
-                nu_preview = '\n\n'.join([prev_text,pname+'\t\t'+ pmodel +'\t\tx'+ qty_input + '\t\t' + str(pprice), purchaseTotal])
+                nu_preview = '\n\n'.join([prev_text,pname+'\t\t'+ pmodel +'\t\tx'+ str(qty_input) + '\t\t' + str(pprice), purchaseTotal])
                 preview.text = nu_preview
         self.ids.disc_input.text = '0.00'
         self.ids.disc_perc_input.text = '0'
         self.ids.after_input.text = str(int(target_code['Before']) -1)
-        self.ids.qty_input.text = qty_input
         self.ids.price_input.text = str(pprice)
         self.ids.total_input.text = str(pprice)
 
-        total= Label(text=str(int(qty_input) * float(target_code['Sale_Price'])), size_hint_x = 0.1, color = (0,0,0.45,1))
-        details.add_widget(total)
 
+        details.add_widget(total)
 
 class OperatorApp(App):
     def build(self):
